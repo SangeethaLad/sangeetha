0968490f52bd7f6aa256c3dbd582d5ada387f215
sangeethaladnm <sangeetha.lad@stayology.com>
2019-08-19 12:50:35 +0530

    changesadded

--- src/movie.js
+++ src/movie.js
@@ -13,43 +13,43 @@ class Movie extends Component {
                     id: 0,
                     imgurl: "http://t2.gstatic.com/images?q=tbn:ANd9GcQA_-tL18_rj9zEcjN6n41NEaJm-kRNF9UeOtvksZ4z_OW6jRA9",
                     title: "AVENGERS",
-                    cast: "AVn",
+                    cast: "2019 ",
                     rating: "4"
                 },
                 {
                     id: 1,
                     imgurl: "http://t0.gstatic.com/images?q=tbn:ANd9GcQhYjUIu2o5v5u3rfJpCq5Cz0Q9WK--XdYxai_N2d0ImohPiIOp",
                     title: "TITANIC",
-                    cast: "TTN",
+                    cast: "1997 ",
                     rating: "5"
                 },
                 {
                     id: 2,
                     imgurl: "http://t0.gstatic.com/images?q=tbn:ANd9GcT_9nrOnN8sYfZZHJ06EIBSoEO5qjx7uHHEs6VtKNplGVuGhZuC",
                     title: "harry potter",
-                    cast: "HAARR",
+                    cast: "2002 ",
                     rating: "6"
                 },
                 {
                     id: 3,
                     imgurl: "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                     title: "AVATAR",
-                    cast: "AVta",
+                    cast: "2009 ",
                     rating: "3"
                 },
                 {
                     id: 4,
                     imgurl: "https://cdn7.newsnation.in/images/2017/04/02/437027723-Bahubalithebeggining_6.jpg",
                     title: "BAHUBALLI",
-                    cast: "Bahu",
+                    cast: "2015 ",
                     rating: "6"
                 },
                 {
                     id: 5,
                     imgurl: "http://t3.gstatic.com/images?q=tbn:ANd9GcQDUBv_iHQkErMkhfMKpklqUN5bo01oetTAvEo8RbZ2BJ-s6O1x",
                     title: "KGF",
-                    cast: "kgf",
-                    rating: "7"
+                    cast: "2018",
+                    rating: "6"
                 }
             ]
         };
@@ -96,16 +96,17 @@ class Movie extends Component {
 
                     {this.state.movies.map(movies => (
                         <div>
-                            <table key={movies.id}>
+                            <table style={{borderCollapse:"collapse"}} key={movies.id}>
                                 <tbody>
+                                
                                     <tr>
                                         <td>
                                             <img alt="poster" width="120" src={movies.imgurl} />
                                         </td>
 
-                                        <td style={{ padding: "15px 20px 20px 76px" }}>{movies.title}</td>
+                                        <td style={{ padding: "15px 20px 20px 76px"}}>{movies.title}</td>
                                         <td style={{ padding: "15px 20px 20px 76px" }}>{movies.cast}</td>
-                                        <td style={{ padding: "15px 20px 20px 76px" }}>{movies.rating}</td>
+                                        <td style={{ padding: "15px 20px 20px 76px"}}>{movies.rating}</td>
                                     </tr>
                                 </tbody>
                             </table>
